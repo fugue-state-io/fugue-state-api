c80e2a5c41e72e11cf5f7b3ba59bf55b97770cba
Jon Zudell <jonathon.zudell@gmail.com>
2023-10-21 22:54:18 +0000

    content length bigger

--- api/src/__init__.py
+++ api/src/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  
+    app.config['MAX_CONTENT_LENGTH'] = 1024 ** 4
     app.register_blueprint(process_audio.bp)
     
     return app
