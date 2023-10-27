8833a884f1c3c16a3fb1a10eeb17a654c4074b59
Jon Zudell <jonathon.zudell@gmail.com>
2023-10-27 01:24:00 +0000

    CORS and better matplotlib

--- api/src/__init__.py
+++ api/src/__init__.py
@@ -1,8 +1,10 @@
 from . import process_audio, readiness
 import os
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = (1024 ** 2) * 50
 app.register_blueprint(process_audio.bp)
 app.register_blueprint(readiness.bp)
+CORS(app)
--- api/src/process_audio.py
+++ api/src/process_audio.py
@@ -4,7 +4,8 @@ import os
 import wave
 import pydub
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.figure as figure
+import uuid
 from io import BytesIO
 from werkzeug.utils import secure_filename
 from flask import Flask, send_file, request, abort, Blueprint, make_response, jsonify
@@ -26,7 +27,6 @@ def process_audio():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             if filename.split('.')[1] != 'wav':
-                plt.clf()
                 temp = tempfile.NamedTemporaryFile()
                 temp.write(file.read())
                 sound = pydub.AudioSegment.from_mp3(temp.name)
@@ -34,16 +34,15 @@ def process_audio():
                 signal_wave = wave.open(temp.name + '.wav', 'r')
                 sample_rate = -1
                 sig = np.frombuffer(signal_wave.readframes(sample_rate), dtype=np.int16)
-                plt.figure(1)
+                fig = figure.Figure()
 
-                plot_a = plt.subplot(211)
+                plot_a = fig.subplots()
                 plot_a.plot(sig, color="g", linewidth=0.25)
                 plot_a.axis('off')
                 
                 tmpfile = BytesIO()
                 
-                plt.savefig(tmpfile, bbox_inches='tight', pad_inches = 0, transparent=True, dpi=256)
-                plt.figure(1).clear()
+                fig.savefig(tmpfile, bbox_inches='tight', pad_inches = 0, transparent=True, dpi=256)
 
                 tmpfile.seek(0)
                 if os.path.isfile(temp.name + '.wav'):
